28417f6b550f21f37ea3fd7df8ceb45ccedf4303
chihuai <s505063@gmail.com>
2014-10-07 08:40:30 +0800

    make quenmgdb can execute test command

--- src/shell.c
+++ src/shell.c
@@ -158,7 +158,7 @@ void test_command(int n, char *argv[]) {
 
     fio_printf(1, "\r\n");
 	 host_action(SYS_SYSTEM, "mkdir output");
-    handle = host_action(SYS_OPEN, "output/syslog", 8);
+    handle = host_action(SYS_OPEN, "output/systest", 8);
     if(handle == -1) {
         fio_printf(1, "Open file error!\n\r");
         return;
