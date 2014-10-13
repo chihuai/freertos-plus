af6afcb31606db18c5f7a1f9ef52586af41a5d9a
chihuai <s505063@gmail.com>
2014-10-13 17:16:19 +0800

    repair the cat command

--- src/romfs.c
+++ src/romfs.c
@@ -11,6 +11,7 @@
 struct romfs_fds_t {
     const uint8_t * file;
     uint32_t cursor;
+    uint32_t size;
 };
 
 
@@ -22,8 +23,7 @@ static uint32_t get_unaligned(const uint8_t * d) {
 
 static ssize_t romfs_read(void * opaque, void * buf, size_t count) {
     struct romfs_fds_t * f = (struct romfs_fds_t *) opaque;
-    const uint8_t * size_p = f->file - 4;
-    uint32_t size = get_unaligned(size_p);
+    uint32_t size = f -> size;
     
     if ((f->cursor + count) > size)
         count = size - f->cursor;
@@ -36,8 +36,7 @@ static ssize_t romfs_read(void * opaque, void * buf, size_t count) {
 
 static off_t romfs_seek(void * opaque, off_t offset, int whence) {
     struct romfs_fds_t * f = (struct romfs_fds_t *) opaque;
-    const uint8_t * size_p = f->file - 4;
-    uint32_t size = get_unaligned(size_p);
+    uint32_t size = f -> size;
     uint32_t origin;
     
     switch (whence) {
@@ -85,13 +84,15 @@ static int romfs_open(void * opaque, const char * path, int flags, int mode) {
     uint32_t h = hash_djb2((const uint8_t *) path, -1);
     const uint8_t * romfs = (const uint8_t *) opaque;
     const uint8_t * file;
+    uint32_t len;
     int r = -1;
 
-    file = romfs_get_file_by_hash(romfs, h, NULL);
+    file = romfs_get_file_by_hash(romfs, h, &len);
 
     if (file) {
         r = fio_open(romfs_read, NULL, romfs_seek, NULL, NULL);
         if (r > 0) {
+        		romfs_fds[r].size = len;
             romfs_fds[r].file = file;
             romfs_fds[r].cursor = 0;
             fio_set_opaque(r, romfs_fds + r);
